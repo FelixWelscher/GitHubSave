8b2a0fe613968f45d9427a5be3caa54994c6f8f6
FelixWelscher <43066226+FelixWelscher@users.noreply.github.com>
2018-09-19 16:17:54 +0300

    Add files via upload

---
+++ LinkedList/src/linkedlist/Node.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlist;
+    
+    
+public class Node {
+    private int value;
+    private Node pointer;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setPointer(Node pointer) {
+        this.pointer = pointer;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getPointer() {
+        return pointer;
+    }
+    
+    
+    
+    
+    
+}
